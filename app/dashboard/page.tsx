46e07e4b8ce11a9bfd99dcfc481cf099cb5ab2ec
ahihi <bdsthocu20@gmail.com>
2023-12-18 17:33:39 +0700

    #5

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,17 @@
+import DropZone from "@/components/DropZone";
+import { auth } from "@clerk/nextjs";
 
 
 
 const Dashboard = () => {
 
+    const { userId } = auth()
+
+
+
     return (
         <div>
-            Dashboard
+            <DropZone />
         </div>
     )
 }
